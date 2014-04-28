f5a1a5166f96740b2f94b75a35ff4d0eb67a34ce
Timoha <gtimoha@gmail.com>
2014-04-28 17:48:26 +0400

    Add a frame

--- index.php
+++ index.php
@@ -4,8 +4,14 @@
 <html>
 	<head>
 		<title>BlackJack Online</title>
+		<link rel="stylesheet" type="text/css" href="/css/main.css" />
+		<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
 	</head>
 	<body>
-		Hi
+		<div class="wrapper">
+			<div id="dealer-block"></div>
+			<div id="table-block"></div>
+			<div id="player-block"></div>
+		</div>
 	</body>
 </html>
