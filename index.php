0a61aa9899ad58cf4325ed63075f1d9e3bb0eb99
Timoha <gtimoha@gmail.com>
2014-04-29 22:26:06 +0400

    Cards html

--- index.php
+++ index.php
@@ -39,10 +39,55 @@ else if (!empty($_POST)) {
 				</div>
 			<?php else : ?>
 				<div id="dealer-block">
+					<h1>Карты дилера</h1>
+					<?php $postiton = 1; ?>
 					<?php foreach ($game->dealer->cards as $card) : ?>
-						<div class="card-block">
-							<h1><?=cardCode($card)?></h1>
+						<?php 
+							$value = $lear = '';
+							switch ($card['value']) {
+								case 1:
+									$value = 'Т';
+								break;
+							
+								case 11:
+									$value = 'В';
+								break;
+							
+								case 12:
+									$value = 'Д';
+								break;
+							
+								case 13:
+									$value = 'К';
+								break;
+							
+								default:
+									$value .= $card['value'];
+								break;
+							}
+							switch ($card['lear']) {
+								case Game::CODE_DIAMONDS:
+									$lear = 'diamonds';
+								break;
+							
+								case Game::CODE_CLUBS:
+									$lear = 'clubs';
+								break;
+							
+								case Game::CODE_SPADES:
+									$lear = 'spades';
+								break;
+							
+								case Game::CODE_HEARTS:
+									$lear = 'hearts';
+								break;
+							}
+						?>
+						<div class="card-block pos<?=$postiton?>">
+							<span><?=$value?></span>
+							<div class="lear <?=$lear?>">&nbsp;</div>
 						</div>
+						<?php $postiton++; ?>
 					<?php endforeach; ?>
 				</div>
 				<div id="player-block">
